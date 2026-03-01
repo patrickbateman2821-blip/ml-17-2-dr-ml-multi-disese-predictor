1da0eb222e8672582f8a5364828cbb6730a8c9dd
Ubuntu <ubuntu@ip-172-31-1-137.ec2.internal>
2026-03-01 21:01:27 +0000

    final product

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.backend.api.routes import router
 
@@ -6,6 +7,14 @@ app = FastAPI(
     title="Dr. ML Prediction App",
     version="1.0.0",
     description="Multi-disease prediction backend"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(router, prefix="/api")
